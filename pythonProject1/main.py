0015866ced5a4693cdd6b67a38bb02f662fde26d
TamerlanKel <tamerlankeligov7883@mail.ru>
2024-07-16 21:55:26 +0300

    Add files via upload

---
+++ pythonProject1/main.py
@@ -0,0 +1,14 @@
+#1st program
+print(9 ** 0.5 * 5)
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3rd program
+a = 1234 // 10 % 100
+b = 5678 // 10 % 100
+print(a + b)
+#4th program
+a = 13.42 // 1
+b = 42.13 // 1
+c = 13.42 // 1 % 0.10
+d = 42.13 % 100
+print(int(c))
