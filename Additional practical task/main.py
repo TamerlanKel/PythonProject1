8b4b5545ae9c1b6d45b7ab846ed6fa33cd84680f
TamerlanKel <tamerlankeligov7883@mail.ru>
2024-07-20 20:50:39 +0300

    Add files via upload

---
+++ Additional practical task/main.py
@@ -0,0 +1,7 @@
+average_score = {"Aaron": [5, 5, 5, 4, 5], "Bilbo": [2, 2, 2, 3], "Johny": [5, 3, 3, 5, 4], "Khendrik": [4, 4, 3], "Steve": [4, 5, 5, 2]}
+average_score["Aaron"] = (5 + 3 + 3 + 5 + 4) / 5
+average_score["Bilbo"] = (2 + 2 + 2 + 3) / 4
+average_score["Johny"] = (5 + 3 + 3 + 5 + 4) / 5
+average_score["Khendrik"] = (4 + 4 + 3) / 3
+average_score["Steve"] = (4 + 5 + 5 + 2) / 4
+print(average_score.items())
