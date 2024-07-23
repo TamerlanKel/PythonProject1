cdcc00780bc21d3d54014caed5367bf80bf9ec68
TamerlanKel <tamerlankeligov7883@mail.ru>
2024-07-23 19:07:23 +0300

    Update main.py

--- pythonProjectModule2.3/main.py
+++ pythonProjectModule2.3/main.py
@@ -10,11 +10,13 @@
 
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 positive_list = []
+
 i = 0
 while i < len(my_list):
     if my_list[i] < 0:
         break
-    else:
+    elif my_list[i] > 0:
         positive_list.append(my_list[i])
     i += 1
+
 print(positive_list)
