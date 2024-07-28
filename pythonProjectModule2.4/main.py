42ba8c7167e718ced4c574c20436eab9896819e3
TamerlanKel <tamerlankeligov7883@mail.ru>
2024-07-28 11:05:14 +0300

    Update main.py

--- pythonProjectModule2.4/main.py
+++ pythonProjectModule2.4/main.py
@@ -23,7 +23,7 @@ for num in (numbers):
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             is_prime = False
-        break
+            break
     if is_prime:
         primes.append(num)
     else:
