164fdf44927010a2b66f0eab5f7507d57fd6d1bf
TamerlanKel <tamerlankeligov7883@mail.ru>
2024-08-07 20:19:36 +0300

    Add files via upload

---
+++ pythonProjectModule4.2/namespaces.py
@@ -0,0 +1,25 @@
+from module1 import d
+
+d = 7
+def square(x):
+
+    d = x ** 2
+    def even(x):
+        nonlocal d
+        d = x / 2
+        if d % 2 == 0:
+            print('Четное')
+        else:
+            print('Нечетное')
+    even(x)
+    return d
+
+
+a = 5
+b = square(4)
+print(a)
+print(b)
+# print(globals())
+
+
+
