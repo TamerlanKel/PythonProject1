281e9d969abb21e9e04e67377a9e93277972ded4
TamerlanKel <tamerlankeligov7883@mail.ru>
2024-09-04 00:09:40 +0300

    main.py

--- pythonProject4/main.py
+++ pythonProject4/main.py
@@ -2,7 +2,7 @@ name = "Тамерлан"
 print("Name:", name)
 age = 21
 print("Age:", age)
-new_age = 22
+new_age = age + 1
 print("New age:", new_age)
 is_student = True
 print("Is Student:", is_student)
