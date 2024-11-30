947acbd6a50a5c36a34ef82fc8cba901f2f286be
TamerlanKel <tamerlankeligov7883@mail.ru>
2024-11-30 19:26:46 +0300

    Update suite_12_3.py

--- pythonProjectsModule12.3/suite_12_3.py
+++ pythonProjectsModule12.3/suite_12_3.py
@@ -1,14 +1,14 @@
 import unittest
 from tests_12_3 import RunnerTest, TournamentTest
 
-# Создаем тестовый набор (TestSuite)
+
 def suite():
     suite = unittest.TestSuite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(RunnerTest))  # Используем loadTestsFromTestCase
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TournamentTest))  # Используем loadTestsFromTestCase
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(RunnerTest)) 
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TournamentTest))  
     return suite
 
-# Запускаем тесты с verbosity=2
+
 if __name__ == "__main__":
-    runner = unittest.TextTestRunner(verbosity=2)  # Установим verbosity=2 для подробного вывода
+    runner = unittest.TextTestRunner(verbosity=2)  
     runner.run(suite())
