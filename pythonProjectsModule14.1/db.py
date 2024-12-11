1891b5ded9124b35110f87d0c4559f12b629e352
TamerlanKel <tamerlankeligov7883@mail.ru>
2024-12-12 00:57:07 +0300

    Update db.py

--- pythonProjectsModule14.1/db.py
+++ pythonProjectsModule14.1/db.py
@@ -21,7 +21,10 @@ for i in range(1, 11):
 
 cursor.execute("UPDATE Users SET balance = 500 WHERE id % 2")
 cursor.execute("DELETE FROM Users WHERE id % 3 = 1")
-cursor.execute("DELETE FROM Users WHERE id == 6")
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
+users = cursor.fetchall()
+for user in users:
+    print(f'Имя: {user[0]}, | Почта: {user[1]}, | Возраст: {user[2]}, | Баланс: {user[3]}')
 
 connection.commit()
 connection.close()
