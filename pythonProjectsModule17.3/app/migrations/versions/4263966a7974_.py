ac1258589aeebd768325f523a99dc699a71845a7
TamerlanKel <tamerlankeligov7883@mail.ru>
2025-02-07 18:50:16 +0300

    Add files via upload

---
+++ pythonProjectsModule17.3/app/migrations/versions/4263966a7974_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 4263966a7974
+Revises: 
+Create Date: 2025-02-07 18:20:41.284640
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '4263966a7974'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
